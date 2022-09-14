e250d5d34872a5adf67a0c9338c55cd570af5563
zyfrontend <frontendzy@gmail.com>
2022-09-14 21:02:08 +0800

    tijiao

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import {
-  BrowserRouter
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./index.css"
+import "normalize.css"
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
